80cb8c88ea5fee414a0b949d14831d6c0849b920
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2024-01-24 14:16:50 -0800

    chore: add astro markdown dependencies and style

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,7 +6,20 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), icon()]
+  image: {
+    domains: ["res.cloudinary.com"],
+  },
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+      wrap: true,
+    },
+  },
 });
