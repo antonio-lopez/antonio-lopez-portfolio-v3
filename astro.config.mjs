9dcf37f36fbef6942315376ef3b64f338d2652e9
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2024-02-10 10:25:03 -0800

    chore: add astro sitemap configuration to build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.antoniolopez.me/",
   image: {
     domains: ["res.cloudinary.com"],
   },
@@ -15,6 +17,7 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     icon(),
+    sitemap(),
   ],
   markdown: {
     shikiConfig: {
